b44ce19e71b3b50296f9bd95544dbaba9b8f2e18
d1inthechamber <daverenz85@gmail.com>
2026-09-11 18:03:11 -0600

    Add blackjack rules test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,5 +42,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.animation:animation")
+    testImplementation("junit:junit:4.13.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
